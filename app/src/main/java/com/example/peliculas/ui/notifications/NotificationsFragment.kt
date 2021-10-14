d1f1b47620e113e84e0c30b448273b627c5ac686
Sergio <sergio-armando-lopez@hotmail.com>
2021-10-14 09:33:01 -0500

    Commit de peliculas 3

---
+++ app/src/main/java/com/example/peliculas/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,76 @@
+package com.example.peliculas.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.peliculas.AdaptadorPeliculas
+import com.example.peliculas.Pelicula
+import com.example.peliculas.R
+import com.example.peliculas.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    var peliculas:ArrayList<Pelicula> = ArrayList();
+    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        agregar_pelicula()
+
+        val lv_series: ListView = root.findViewById(R.id.listView_Favoritos)
+
+        val adaptador= AdaptadorPeliculas(root.context,peliculas)
+
+        lv_series.adapter = adaptador
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    fun agregar_pelicula(){
+        peliculas.add(
+            Pelicula (1, "Tenet", "Armado con una única palabra, Tenet, y luchando por la supervivencia del mundo, el protagonista viaja por el oscuro mundo del espionaje internacional  en una misión  que irá más allá del tempo real",
+            R.drawable.tenet
+        )
+        )
+        peliculas.add(
+            Pelicula (2,  "Origen",  "A ve ladrón que roba secretos corporativos a través del uso de la tecnología de compartir sueños, se le da la tarea de implantar  una idea de la mente  en un jefe  de una gran empresa",
+            R.drawable.inception
+        )
+        )
+        peliculas.add(
+            Pelicula( 3,  "Interstellar",  "Un equipo de exploradores viaja a través de un agujero de gusano en el espacio en un intento de garantizar  la supervivencia de la humanidad",
+            R.drawable.interstellar
+        )
+        )
+        peliculas.add(
+            Pelicula( 4,  "El prestigio",  "Tras un trágico accidente, dos magos profesionales empiezan una batalla para crear el mejor truco, sacrificando todo en sus vidas para superarse el uno al otro",
+            R.drawable.theprestige
+        )
+        )
+    }
+
+}
